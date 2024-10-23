c3d31eb12de07a36b190f6c49e10b0c39053f0fd
Aisha Yacoob <aishayacoob@Aishas-MacBook-Air.local>
2024-10-23 16:35:07 -0400

    added decoding function

--- lab6.py
+++ lab6.py
@@ -1,32 +1,44 @@
 #Nicolas Broderick
 
-
 def encode():
-	return_string = ''
-	info = input('Please enter your password to decode: ')
-	for i in info:
-		j = int(i) + 3
-		return_string += str(j)
-	return return_string
+    return_string = ''
+    info = input('Please enter your password to encode: ')
+    for i in info:
+        j = int(i) + 3
+        return_string += str(j)
+    return return_string
 
-def main():
-	choice = 3.14159
+#Aisha Yacoob
+def decode(encoded_password):
+    return_string = ''
+    for i in encoded_password:
+        j = int(i) - 3
+        return_string += str(j)
+    return return_string
 
-	while choice != 3:
-		print('\nMenu')
-		print('-------------')
-		print('''1. Encode
+def main(): 
+    choice = 3.14159
+    encoded_pword = ""
+
+    while choice != 3:
+        print('\nMenu')
+        print('-------------')
+        print('''1. Encode
 2. Decode
 3. Quit''')
-		choice = int(input('\nPlease enter an option: '))
+        choice = int(input('\nPlease enter an option: '))
 
-		if choice == 1:
-			pword = encode()
-			print('Your password has been encoded and stored!')
-		elif choice == 2:
-			pass
-		elif choice == 3:
-			break
+        if choice == 1:
+            encoded_pword = encode()
+            print('Your password has been encoded and stored!')
+        elif choice == 2:
+            if encoded_pword:
+                original_pword = decode(encoded_pword)
+                print(f'The encoded password is {encoded_pword}, and the original password is {original_pword}.')
+            else:
+                print("No password has been encoded yet.")
+        elif choice == 3:
+            break
 
 if __name__ == '__main__':
-	main()
+    main()
